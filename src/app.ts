74131409540911d257f6d9db59070595cbb31d7f
KawsarRobin <kawsarrobin3@gmail.com>
2023-06-02 19:06:36 +0600

    setup eslint husky, lintstage prettier

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,16 @@
-import cors from 'cors';
-import express, { Application, Request, Response } from 'express';
-const app: Application = express();
-const port = 3000;
+import cors from 'cors'
+import express, { Application, Request, Response } from 'express'
+const app: Application = express()
 
-app.use(cors());
+app.use(cors())
 
-// parser
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// parser used
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 // testing
 app.get('/', (req: Request, res: Response) => {
-  res.send('Working successfully!');
-});
+  res.send('Working successfully!')
+})
 
-export default app;
+export default app
