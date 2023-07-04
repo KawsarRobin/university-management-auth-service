0535e77bce1c44b345154e5b19f51763a341917f
KawsarRobin <kawsarrobin3@gmail.com>
2023-07-04 22:25:25 +0600

    module 17 done

--- src/server.ts
+++ src/server.ts
@@ -1,47 +1,47 @@
-import { Server } from 'http'
-import mongoose from 'mongoose'
-import app from './app'
-import config from './config/index'
-import { errorLogger, logger } from './shared/logger'
+import { Server } from 'http';
+import mongoose from 'mongoose';
+import app from './app';
+import config from './config/index';
+import { errorLogger, logger } from './shared/logger';
 
 //uncaughtException handle
 process.on('uncaughtException', error => {
-  errorLogger.error(error)
-  process.exit(1)
-})
+  errorLogger.error(error);
+  process.exit(1);
+});
 
-let server: Server
+let server: Server;
 async function bootstrap() {
   try {
-    await mongoose.connect(config.database_url as string)
-    logger.info('Database connected successfully')
+    await mongoose.connect(config.database_url as string);
+    logger.info('Database connected successfully');
 
     server = app.listen(config.port, () => {
-      logger.info(`Application listening on port ${config.port}`)
-    })
+      logger.info(`Application listening on port ${config.port}`);
+    });
   } catch (error) {
-    errorLogger.error('something went wrong connecting database', error)
+    errorLogger.error('something went wrong connecting database', error);
   }
 
   //unhandledRejection handle
   process.on('unhandledRejection', error => {
     if (server) {
       server.close(() => {
-        errorLogger.error(error)
-        process.exit(1)
-      })
+        errorLogger.error(error);
+        process.exit(1);
+      });
     } else {
-      process.exit(1)
+      process.exit(1);
     }
-  })
+  });
 }
-bootstrap()
+bootstrap();
 
 //SIGTERM is sending signal about when Server was suddenly off.
 //handle SIGTERM
 process.on('SIGTERM', () => {
-  logger.info('SIGTERM is received')
+  logger.info('SIGTERM is received');
   if (server) {
-    server.close()
+    server.close();
   }
-})
+});
